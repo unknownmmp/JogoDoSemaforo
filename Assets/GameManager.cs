4e3cf8adaa38d7396765ee3d20f952b0ffc3315b
PC_01\unknmmp <unknown.mmp@gmail.com>
2017-07-15 14:32:19 +0100

    no message

---
+++ Assets/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public Sprite verde;
+    public Sprite amarelo;
+    public Sprite vermelho;
+    public Color color;
+    public Image image;
+
+    int state = 0;
+
+    void Start() 
+    {
+        image = GetComponent<Image>(); 
+    }
+
+    public void ButtonClicked ()
+    {
+        //0-nada 1-verde 2-amarelo 3-vermelho
+        if(state != 3) state++;
+        CheckState();
+    }
+
+    void CheckState ()
+    {
+        Debug.Log("DEBUG: State: " + state);
+        if(state != 0)
+        {
+            image.color = color;
+        }
+        if (state == 1) image.overrideSprite = verde;
+        else if (state == 2) image.overrideSprite = amarelo;
+        else if (state == 3) image.overrideSprite = vermelho;
+        else image.overrideSprite = null;
+
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
